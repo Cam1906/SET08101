0189a0e4821179256fcf126a5e8f9434c33d46a4
Cam1906 <123465742+Cam1906@users.noreply.github.com>
2023-04-14 15:59:28 +0100

    Add files via upload

---
+++ JS/searchInput.js
@@ -0,0 +1,39 @@
+var input = document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      console.log("Test")
+      checkKeyword();
+    }
+  });
+
+function checkKeyword() {
+    var input = document.getElementById("search-input").value.toLowerCase();
+    var keywords_cheese = ["classic cheese", "classic", "cheese", "original", "cheddar"];
+    var keywords_bbq = ["bangin bbq", "bangin", "bbq", "bbq sauce", "pork", "pulled pork", "smoked", "smokey"];
+    var keywords_chicken = ["crispy chicken", "crispy", "chicken", "fried", "mayo"];
+    var keywords_plant = ["plant powered", "plant", "vegetarian", "veg", "veggie", "healthy", "meat free"];
+    var keywords_inferno = ["inferno", "hot", "spicy", "firey", "chili", "jalapeno", "chipotle"];
+    var keywords_mediterranean = ["mediterranean", "greek", "lamb", "Hummus", "sweetcorn", "zucchini"];
+    
+    if (keywords_cheese.includes(input)) {
+        window.location.href = "../HTML/ClassicCheese.html";
+    }
+    else if (keywords_bbq.includes(input)) {
+        window.location.href = "../HTML/BanginBBQ.html";
+    }
+    else if (keywords_chicken.includes(input)) {
+        window.location.href = "../HTML/CrispyChicken.html";
+    }
+    else if (keywords_plant.includes(input)) {
+        window.location.href = "../HTML/PlantPowered.html";
+    }
+    else if (keywords_inferno.includes(input)) {
+        window.location.href = "../HTML/Inferno.html";
+    }
+    else if (keywords_mediterranean.includes(input)) {
+        window.location.href = "../HTML/Mediterranean.html";
+    } 
+    else {
+        alert("No match found");
+    }
+}  
